21c26e2b36bc5a4927b172b38c8e506cc8419873
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-11-04 09:58:39 +0300

    Finished all tasks

--- arrays.js
+++ arrays.js
@@ -2,30 +2,35 @@
 // Write a createGroceries function that returns an array with 6 groceries items
 function createGroceries() {
   // write your code here...
+  return ["Apple", "Orange", "Cheese", "Milk", "Bread", "Egg"];
 }
 
 // Step 2:
 // Write a getSecondGroceryItem function that returns the second grocery item from `groceries` argument
 function getSecondGroceryItem(groceries) {
   // write your code here...
+  return groceries[1];
 }
 
 // Step 3:
 // Write a getGroceriesCount that returns the length of the `groceries` argument
 function getGroceriesCount(groceries) {
   // write your code here...
+  return groceries.length;
 }
 
 // Step 4:
 // Write a getLastGroceryItem function that returns the last grocery item from `groceries` argument
 function getLastGroceryItem(groceries) {
   // write your code here...
+  return groceries[groceries.length - 1];
 }
 
 // Step 5:
 // Write a removeLastGroceryItem function that removes the last grocery item and return it
 function removeLastGroceryItem(groceries) {
   // write your code here...
+  return groceries.pop();
 }
 
 // Step 6:
@@ -33,12 +38,15 @@ function removeLastGroceryItem(groceries) {
 // and returns the modified array
 function addNewGroceries(groceries, itemOne, itemTwo) {
   // write your code here...
+  groceries.push(itemOne, itemTwo);
+  return groceries;
 }
 
 // Step 7:
 // Write a getFirstThreeGroceryItems function that returns a new array that contains the first three grocery items
 function getFirstThreeGroceryItems(groceries) {
   // write your code here...
+  return groceries.slice(0, 3);
 }
 
 // 🌶️🌶️🌶️ **Challenge**
@@ -48,6 +56,8 @@ function getFirstThreeGroceryItems(groceries) {
 // and returns the modified groceries list
 function deleteThirdItem(groceries) {
   // write your code here...
+  groceries.splice(2, 1);
+  return groceries;
 }
 
 // Step 2:
@@ -55,6 +65,8 @@ function deleteThirdItem(groceries) {
 // and returns the modified groceries array
 function insertItemAtBeginning(groceries, item) {
   // write your code here...
+  groceries.unshift(item);
+  return groceries;
 }
 
 // Step 3:
@@ -62,10 +74,14 @@ function insertItemAtBeginning(groceries, item) {
 // and return the modified groceries array
 function replaceFirstTwoItems(groceries) {
   // write your code here...
+  groceries.splice(0, 2, "ketchup", "chili");
+  return groceries;
 }
 
 // DO NOT CHANGE THE LINE OF CODE BELOW
 const groceries = createGroceries();
+
+// console.log(getSecondGroceryItem(groceries));
 
 module.exports = {
   groceries,
